2a2bad83f4b699eb4cb20497f4b3806259ce7b50
josemca1105 <jm.calderon1105@gmail.com>
2026-04-16 20:22:53 -0400

    class 06 finished

--- 01-primeros_pasos/06-string_formatting.py
+++ 01-primeros_pasos/06-string_formatting.py
@@ -1,7 +1,11 @@
 # Concatenar strings
 name = "Jose"
 last_name = "Calderon"
+age = 23
 
-full_name = name + " " + last_name # funcional pero no es buena practica
+# full_name = name + " " + last_name # funcional pero no es buena practica
 
-print(full_name)
+full_name = f"{name} {last_name}" # mejor practica, mas legible y facil de mantener
+message = f"{full_name}, tienes {age} años" # tambien se pueden incluir variables dentro de la cadena
+
+print(message) # Jose Calderon
