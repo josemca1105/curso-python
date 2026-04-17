c65e43c8f14acc78b62fa9dd90ef0d474a7edddb
josemca1105 <jm.calderon1105@gmail.com>
2026-04-16 20:47:05 -0400

    string indexes practice

--- 01-primeros_pasos/07-string_indexes.py
+++ 01-primeros_pasos/07-string_indexes.py
@@ -1,4 +1,4 @@
-name = "Manuel" # 0-M 1-a 2-n 3-u 4-e 5-l
+# name = "Manuel" # 0-M 1-a 2-n 3-u 4-e 5-l
 # print(name) # Manuel
 
 # print(name[0]) # M
@@ -12,7 +12,14 @@ name = "Manuel" # 0-M 1-a 2-n 3-u 4-e 5-l
 # [Start:Stop]
 # Manuel
 # Ma
-print(name[0:2]) # Ma
+# print(name[0:2]) # Ma
 
 # [Start:Stop:stepover]
-print(name[0:3:2]) # Mn
+# print(name[0:3:2]) # Mn
+
+# Como puedo poner mi nombre al reves?
+name = "Jose"
+
+name_reversed = name[::-1]
+
+print(name_reversed) # esoJ
