c2a29a9b72cfdb802d6dd03f83ef685912c299b2
josemca1105 <jm.calderon1105@gmail.com>
2026-04-16 20:39:45 -0400

    class 07 finished

---
+++ 01-primeros_pasos/07-string_indexes.py
@@ -0,0 +1,18 @@
+name = "Manuel" # 0-M 1-a 2-n 3-u 4-e 5-l
+# print(name) # Manuel
+
+# print(name[0]) # M
+# print(name[1]) # a
+#...
+# print(name[3]) # u
+
+# Como obtener la ultima letra?
+# print(name[-1])
+
+# [Start:Stop]
+# Manuel
+# Ma
+print(name[0:2]) # Ma
+
+# [Start:Stop:stepover]
+print(name[0:3:2]) # Mn
