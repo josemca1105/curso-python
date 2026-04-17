00b7c90a777bbe5c21a232b79ae29f93a311cef1
josemca1105 <jm.calderon1105@gmail.com>
2026-04-16 21:00:34 -0400

    added and finished class 08

---
+++ 01-primeros_pasos/08-immutable.py
@@ -0,0 +1,13 @@
+# Que es inmutabilidad? Significa que algo no puede ser cambiado
+
+name = "Jose"
+# name[0] = "C"  # Esto causará un error
+# print(name)
+
+name = "Manuel"
+print(name)
+
+'''
+Los objetos inmutables no pueden ser cambiados,
+pero si podemos crear un nuevo objeto a partir del original
+'''
