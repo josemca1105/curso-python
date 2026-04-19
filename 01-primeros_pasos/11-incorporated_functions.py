4623b720428a6e85264237df4dc39f25da5ff27e
josemca1105 <jm.calderon1105@gmail.com>
2026-04-19 13:54:23 -0400

    class 11 incorporated functions

---
+++ 01-primeros_pasos/11-incorporated_functions.py
@@ -0,0 +1,13 @@
+
+# Funciones incorporadas - Funciones nativas
+print("Hola Mundo") # Hola Mundo
+type(100) # int
+str(100) # '100'
+round(20.9) # 21
+abs(-20) # 20
+
+# Metodos - Tareas especificas
+message = 'Es un buen programador'
+new_message = message.replace('Es', 'Soy')
+
+print(new_message) # Soy un buen programador
