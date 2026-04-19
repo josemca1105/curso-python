1f3c6da17385a9b81a056e9a39da82596feda2c4
josemca1105 <jm.calderon1105@gmail.com>
2026-04-19 11:30:35 -0400

    class 10 math operations

---
+++ 01-primeros_pasos/10-math_operations.py
@@ -0,0 +1,30 @@
+# Suma
+print(2 + 2) # 4
+
+# Resta
+print(4 -2) # 2
+
+# Multiplicación
+print(4 * 2) # 8
+
+# Division
+print(2 / 4) # 0.5
+
+
+# Otras operaciones
+
+# Potencia
+print(2 ** 3) # 8
+
+# Division sin decimales
+print(5 // 4) # 1
+
+# Modulo (lo que sobra de la division: Residio)
+print(6 % 4) # 2
+
+# Redondear: round()
+print(round(3.1)) # 3
+print(round(3.9)) # 4
+
+# Valor absoluto: abs()
+print(abs(-20)) # 20
