a7089ce3d0cdb526a9e8d5d6f815b21202934383
josemca1105 <jm.calderon1105@gmail.com>
2026-04-19 11:20:18 -0400

    class 09 convertions

---
+++ 01-primeros_pasos/09-convertions.py
@@ -0,0 +1,40 @@
+value = 100
+type_value = type(value)
+
+# print(value)
+# print(type_value)
+
+# Int a Str
+value = str(100) + 'Hola'
+type_value = type(value)
+
+# print(value)
+# print(type_value)
+
+# Str a Int
+value = int('100')
+type_value = type(value)
+
+print(value)
+print(type_value)
+
+# Str a Float
+value = float('100')
+type_value = type(value)
+
+print(value)
+print(type_value)
+
+# Int a Float
+value = float(100)
+type_value = type(value)
+
+print(value)
+print(type_value)
+
+# Float a Int
+value = int(100.999)
+type_value = type(value)
+
+print(value)
+print(type_value)
