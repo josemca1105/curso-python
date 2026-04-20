468e3d820e1e99fe11f9f1c4e8ffce3d08e1398d
josemca1105 <jm.calderon1105@gmail.com>
2026-04-20 19:51:50 -0400

    section 2 class 01 if conditional

---
+++ 02-condicionales/01-if_conditional.py
@@ -0,0 +1,13 @@
+# is_old = False
+
+# if is_old:
+#     print("Puedes manejar")
+# else:
+#     print("Espera cumplir la mayoria de edad")
+
+is_student = True
+
+if is_student:
+    print("Obten la licencia de estudiante")
+else:
+    print("Lo siento, necesitas comprar una licencia normal")
