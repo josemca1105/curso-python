efb2cf75140b31260f3c9a76c64d72da015f7e16
josemca1105 <jm.calderon1105@gmail.com>
2026-04-20 20:07:12 -0400

    class 2 elif conditional

---
+++ 02-condicionales/02-elif_conditional.py
@@ -0,0 +1,9 @@
+is_old = False
+is_licenced = False
+
+if is_old:
+    print("Tienes edad para manejar")
+elif is_licenced:
+    print("Puedes manejar con tu licencia en la ciudad")
+else:
+    print("Espera a cumplir la mayoria de edad o tramita tu licencia")
