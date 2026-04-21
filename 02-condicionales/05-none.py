f126b39002cbdc1d6fad81ed6502566c347fbb72
josemca1105 <jm.calderon1105@gmail.com>
2026-04-20 20:43:04 -0400

    class 04 none

---
+++ 02-condicionales/05-none.py
@@ -0,0 +1,12 @@
+# print(bool(None))
+
+# None - vacio, nada, ausencia de valor
+
+# Null == None / Null no existe en Python
+
+user = None
+
+if user:
+    print("Este usuario esta registrado")
+else:
+    print("Usuario disponible")
