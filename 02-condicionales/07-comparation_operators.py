b85e5e5f0174d7a593b4b86c02bf64df6ec21d4f
josemca1105 <jm.calderon1105@gmail.com>
2026-04-21 20:10:52 -0400

    class 07 comparation operators

---
+++ 02-condicionales/07-comparation_operators.py
@@ -0,0 +1,31 @@
+# Igualdad ==
+print(5 == 5)  # True
+print(5 == 3) # False
+print("hello" == "hello") # True
+print("hello" == "Hello") # False
+
+# Desigualdad !=
+print(5 != 5)  # False
+print(5 != 3) # True
+print("hello" != "hello") # False
+print("hello" != "Hello") # True
+
+# Mayor que >
+print(7 > 5) # True
+print(5 > 9) # False
+
+# Menor que <
+print(7 < 5) # False
+print(5 < 9) # True
+
+# Mayor o igual que >=
+print(7 >= 5) # True
+print(5 >= 9) # False
+# print(5 > 5) # False
+print(5 >= 5) # True
+
+# Menor o igual que <=
+print(7 <= 5) # False
+print(5 <= 9) # True
+# print(5 < 5) # False
+print(5 <= 5) # True
