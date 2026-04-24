b481814796432fbc1fe96bffe47b524a1b8414df
josemca1105 <jm.calderon1105@gmail.com>
2026-04-23 20:40:55 -0400

    section 3 class 01 lists

---
+++ 03-listas/01-lists.py
@@ -0,0 +1,18 @@
+# Una lista es una colección de elementos ordenados y mutables. Se pueden modificar después de su creación,
+# lo que las hace muy útiles para almacenar datos que pueden cambiar a lo largo del tiempo.
+
+# list es una palabra reservada en Python que se utiliza para crear una lista.
+# Las listas se definen utilizando corchetes [] y los elementos se separan por comas.
+
+list_numbers = [1, 2, 3, 4, 5]
+list_letters = ['a', 'b', 'c']
+
+list_mix = [2, 'z', True, [1, 2, 3], 10.5]
+
+# Ejemplo de la vida real
+
+shopping_cart = ['Laptop', 'Silla Gamer', 'Cafe']
+
+# print(shopping_cart)
+# print(list_mix)
+print(type(list_mix))
