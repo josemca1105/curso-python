03f21e125817588a54d722fda7eb861366763ec2
josemca1105 <jm.calderon1105@gmail.com>
2026-05-25 11:50:07 -0400

    class 05 search methods

---
+++ 03-listas/05-list_methods_search.py
@@ -0,0 +1,11 @@
+numbers_list = [1, 2, 3, 4, 5, 2, 5, 9, 7, 8, 9, 9, 1, 2]
+
+# index() - devuelve el índice de la primera aparición del valor especificado
+# print(numbers_list.index(3, 0, 2))
+
+# in - devuelve True si el valor especificado se encuentra en la lista, de lo contrario devuelve False
+# print(3 in numbers_list)
+# print(10 in numbers_list)
+
+# count() - devuelve el número de veces que el valor especificado aparece en la lista
+print(numbers_list.count(2))
