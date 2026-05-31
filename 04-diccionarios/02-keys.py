bac5171e6f54932818536b1cbd95d287ec67b778
josemca1105 <jm.calderon1105@gmail.com>
2026-05-31 10:55:52 -0400

    class 02 dictionary keys

---
+++ 04-diccionarios/02-keys.py
@@ -0,0 +1,13 @@
+user = {
+    "name": "Jose",
+    "age": 24,
+    "email": "jose@gmail.com",
+    (20.85, -67.67): "Valencia"
+}
+
+print(user)
+
+user["name"] = "Manuel"
+user["age"] = 25
+user["country"] = "Venezuela"
+print(user[(20.85, -67.67)])
