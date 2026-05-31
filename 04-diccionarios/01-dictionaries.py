a99d7adf8d68cd6a2a1995dc0ed4ac3bf382ce0f
josemca1105 <jm.calderon1105@gmail.com>
2026-05-31 10:47:59 -0400

    seciont 4 dictionaries class 1

---
+++ 04-diccionarios/01-dictionaries.py
@@ -0,0 +1,19 @@
+# Diccionarios = Objeto (JSON)
+# No estan ordenados
+
+dictionary = {
+    "key": "value",
+    "a": [1, 2, 3],
+    "b": 2,
+    "verdad": True
+}
+
+# print(dictionary["verdad"])
+
+user = {
+    "name": "Jose",
+    "country": "Venezuela",
+    "age": 24
+}
+
+print(user["name"])
