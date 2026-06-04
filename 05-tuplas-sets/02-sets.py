d1b7f2bb54818b7bf70990c5ec0922d18de9b3df
Jose Calderon <jm.calderon1105@gmail.com>
2026-06-04 19:35:30 -0400

    class 02 sets

---
+++ 05-tuplas-sets/02-sets.py
@@ -0,0 +1,14 @@
+my_set = {1, 2, 3, 5, 4, 3, 2, 1, 3, 9, 7, 3, 8}
+
+# es una coleccion desornedada
+# no tiene indices
+# no permite duplicados
+# optimo para operaciones matematicas
+
+# print(my_set)
+
+# dnis = {123, 345, 743, 153, 232, 123, 123}
+# print(dnis)
+
+usernames = {"jose123", "manuel321", "vergil", "vergil"}
+print(usernames)
