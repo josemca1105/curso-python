e2329305800f854b82ba594efd66654d97407b9f
josemca1105 <jm.calderon1105@gmail.com>
2026-07-03 12:24:36 -0400

    section 6 first calls for loop

---
+++ 06-loops/01-for_loop.py
@@ -0,0 +1,15 @@
+# ciclo for
+# for item in 'Devtalles':
+#     print(item)
+
+# fruits = ['Mango', 'Fresa', 'Durazno']
+# for fruit in fruits:
+#     print(fruit)
+
+my_list = [1, 2, 3, 4, 5]
+sum = 0
+for number in my_list:
+    print(number)
+    sum += number
+
+print(sum)
