18b50ee08a0e9973ec567b753cb932f5c6e1d9ac
josemca1105 <jm.calderon1105@gmail.com>
2026-07-03 13:55:26 -0400

    class 4 enumerate on for loop

---
+++ 06-loops/04-enumerate.py
@@ -0,0 +1,10 @@
+# for index, char in enumerate('Devtalles'):
+# print(index, char)
+
+# for index, number in enumerate([1, 2, 3, 4, 5]):
+#     print(index, number)
+
+for index, number in enumerate(list(range(100))):
+    print(index, number)
+    if number == 30:
+        print("Has llegado al numero 30 :D")
