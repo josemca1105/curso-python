ccfd6170fc992ddd3e5b09a565a5d162d2ceeb74
josemca1105 <jm.calderon1105@gmail.com>
2026-07-03 11:40:36 -0400

    class 7 convert data types

---
+++ 05-tuplas-sets/07-converts.py
@@ -0,0 +1,14 @@
+list1 = [1, 2, 3, 4, 8, 5, 7, 5, 8, 9, 7, 7, 8, 6]
+tuple1 = tuple(list1)  # convertir lista a tupla
+
+# print(list1)
+# print(tuple1)
+
+set1 = set(list1)  # convertir lista a conjunto
+# print(set1)
+
+
+list_tuple = [('a', 1), ('b', 2), ('c', 3)]
+dictionary = dict(list_tuple)  # convertir lista de tuplas a diccionario
+print(list_tuple)
+print(dictionary)
