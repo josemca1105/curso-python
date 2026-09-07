ea36611eb719c59343c16fafebe36fd4ae01229a
josemca1105 <jm.calderon1105@gmail.com>
2026-09-07 11:53:32 -0400

    clase 2 funcion return

---
+++ 07-functions/02-return.py
@@ -0,0 +1,13 @@
+def hello():
+    print("Hola Mundo desde una Funcion :D")
+    
+    return 2 + 2
+    
+def bye():
+    print("Adios Mundo desde una Funcion D:")
+    return True
+
+# hello()
+# bye()
+print(hello())
+print(bye())
