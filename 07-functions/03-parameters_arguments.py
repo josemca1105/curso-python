1d663d8c1de61235b51b3142c8916da0f880b111
josemca1105 <jm.calderon1105@gmail.com>
2026-09-07 12:27:08 -0400

    clase 3 parametros y argumentos

---
+++ 07-functions/03-parameters_arguments.py
@@ -0,0 +1,7 @@
+# parametros: van dentro de los parentesis, y son los que va a recibir la funcion. siempre van al momento de definir la funcion
+def hello(greet, name):
+    print(f"{greet}, {name}")
+
+# argumentos: van dentro de los parentesis al momento de hacer la llamada
+hello("Buen dia", "Jose")
+hello("Hello", "Calderon")
