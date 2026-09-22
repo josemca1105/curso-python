7302b1920f1f063d157cde87ea98b761c3bccf68
josemca1105 <jm.calderon1105@gmail.com>
2026-09-21 20:59:29 -0400

    clase 8 palabra reservada nonlocal en funciones

---
+++ 07-functions/08-nonlocal_keywords.py
@@ -0,0 +1,11 @@
+def outer():
+    enclosing_variable = "Enclosing variable"
+    
+    def inner():
+        nonlocal enclosing_variable
+        enclosing_variable = "Enclosing modificado"
+    
+    inner()
+    print(enclosing_variable)
+
+outer()
