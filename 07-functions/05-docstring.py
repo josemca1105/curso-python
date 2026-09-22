260b1ab746f8206a8a60f37816bc00ac49305fca
josemca1105 <jm.calderon1105@gmail.com>
2026-09-21 20:38:22 -0400

    clase 5 explicacion docstring

---
+++ 07-functions/05-docstring.py
@@ -0,0 +1,18 @@
+# DOCSTRING
+# def hello(greet="Hola", name="estimado"):
+#     """
+#     Info: Esta es una funcion para devolver un saludo personalizado.
+#     Devuelve un string o un print.
+#     """
+#     print(f"{greet}, {name}")
+
+# hello(name="Jose", greet="Hello")
+
+def multiply(a: float, b: float) -> float:
+    """
+    Info: Multiplica dos numeros y devuelve el resultado.
+    """
+    
+    return a * b
+
+print(multiply(15.4, 3.98))
